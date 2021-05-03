0701f86b9c4db4d7b05879c6b1283dcd52579b72
Krishanus-2001 <krishanu21saini@gmail.com>
2021-05-03 21:22:53 +0530

    Add flash and error page

--- models/admin.py
+++ models/admin.py
@@ -10,4 +10,9 @@ app.config.from_object('config.DevConfig')
 mysql = MySQL(app)
 
 def admin_dashboard():
+    if 'id' not in session or 'role' not in session:
+        return render_template('error.html')
+    elif session['role'] != "admin":
+        return render_template('error.html')
+
     return render_template('admin/dashboard.html')
--- models/faculty.py
+++ models/faculty.py
@@ -10,4 +10,9 @@ app.config.from_object('config.DevConfig')
 mysql = MySQL(app)
 
 def faculty_dashboard():
+    if 'id' not in session or 'role' not in session:
+        return render_template('error.html')
+    elif session['role'] != "faculty":
+        return render_template('error.html')
+        
     return render_template('faculty/dashboard.html')
