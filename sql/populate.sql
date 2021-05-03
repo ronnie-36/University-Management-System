54eba3ec4027c5bef4d75711a904b57dbcf09f66
Rahul Kumar <rahulkrkr123@gmail.com>
2021-05-03 12:48:47 +0530

    sql updates

---
+++ sql/populate.sql
@@ -0,0 +1,10 @@
+-- Insert valid emails accordingly for testing forgot password
+INSERT INTO `university`.`faculty` (`faculty_id`, `name`, `emailid`, `password`) VALUES ('1', 'qwerty', 'asdf@asdf.com', '81dc9bdb52d04dc20036dbd8313ed055');
+INSERT INTO `university`.`faculty` (`faculty_id`, `name`, `emailid`, `password`) VALUES ('2', 'auwo', 'auwo@auwo.com', '81dc9bdb52d04dc20036dbd8313ed055');
+
+INSERT INTO `university`.`student` (`student_id`, `name`, `emailid`, `password`) VALUES ('1', 'ron', 'ron@ron.com', '81dc9bdb52d04dc20036dbd8313ed055');
+INSERT INTO `university`.`student` (`student_id`, `name`, `emailid`, `password`) VALUES ('2', 'alex', 'alex@alex.com', '81dc9bdb52d04dc20036dbd8313ed055');
+
+INSERT INTO `university`.`admin` (`admin_id`, `name`, `emailid`, `password`) VALUES ('1', 'sqyw', 'sqyw@sqyw.com', '81dc9bdb52d04dc20036dbd8313ed055');
+INSERT INTO `university`.`admin` (`admin_id`, `name`, `emailid`, `password`) VALUES ('2', 'qwsr', 'qwsr@qwsr.com', '81dc9bdb52d04dc20036dbd8313ed055');
+
