8ae5a08bacb20776cb0a039635cd6a78dd2369b5
Krishanus-2001 <krishanu21saini@gmail.com>
2021-05-07 15:11:43 +0530

    Add sql

--- sql/populate.sql
+++ sql/populate.sql
@@ -1,9 +1,9 @@
 -- Insert valid emails accordingly for testing forgot password
-INSERT INTO `university`.`faculty` (`faculty_id`, `name`, `emailid`, `password`) VALUES ('1', 'qwerty', 'asdf@asdf.com', '81dc9bdb52d04dc20036dbd8313ed055');
-INSERT INTO `university`.`faculty` (`faculty_id`, `name`, `emailid`, `password`) VALUES ('2', 'auwo', 'auwo@auwo.com', '81dc9bdb52d04dc20036dbd8313ed055');
+INSERT INTO `university`.`faculty` (`faculty_id`, `name`, `emailid`, `password`, `address`, `DOB`) VALUES ('1', 'Shaan', 'sh@sh.com', '81dc9bdb52d04dc20036dbd8313ed055', '02-gandhi-marg new-delhi', '1980-12-30');
+INSERT INTO `university`.`faculty` (`faculty_id`, `name`, `emailid`, `password`, `address`, `DOB`) VALUES ('2', 'Rohan', 'rr@trr.com', '81dc9bdb52d04dc20036dbd8313ed055', '02-Hauz-Khas new-delhi', '1965-12-30');
 
-INSERT INTO `university`.`student` (`student_id`, `name`, `emailid`, `password`) VALUES ('1', 'ron', 'ron@ron.com', '81dc9bdb52d04dc20036dbd8313ed055');
-INSERT INTO `university`.`student` (`student_id`, `name`, `emailid`, `password`) VALUES ('2', 'alex', 'alex@alex.com', '81dc9bdb52d04dc20036dbd8313ed055');
+INSERT INTO `university`.`student` (`student_id`, `name`, `emailid`, `password`, `address`, `DOB` , `branch`, `sem`, `cpi`) VALUES ('1', 'ron', 'ron@ron.com', '81dc9bdb52d04dc20036dbd8313ed055', '100-england', '2001-01-01', 'CSE', '4', '9.80');
+INSERT INTO `university`.`student` (`student_id`, `name`, `emailid`, `password`, `address`, `DOB` , `branch`, `sem`, `cpi`) VALUES ('2', 'Ganguly', 'Ganguly@Ganguly.com', '81dc9bdb52d04dc20036dbd8313ed055', '100-london', '2004-02-21', 'EE', '2', '7.80');
 
 INSERT INTO `university`.`admin` (`admin_id`, `name`, `emailid`, `password`) VALUES ('1', 'sqyw', 'sqyw@sqyw.com', '81dc9bdb52d04dc20036dbd8313ed055');
 INSERT INTO `university`.`admin` (`admin_id`, `name`, `emailid`, `password`) VALUES ('2', 'qwsr', 'qwsr@qwsr.com', '81dc9bdb52d04dc20036dbd8313ed055');
@@ -14,11 +14,16 @@ INSERT INTO course (c_id, name, year, notes) VALUES ('3','DSA',"2006-06-24","");
 INSERT INTO course (c_id, name, year, notes) VALUES ('4','Software Engg',"2009-01-30","");
 INSERT INTO course (c_id, name, year, notes) VALUES ('5','IC211',"2009-01-01","");
 
-INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('1','1',9.50,"");
-INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('1','2',8.50,"");
-INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('1','3',4.00,"");
-INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('2','1',10.00,"");
-INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('2','2',9.00,"");
+INSERT INTO section (c_id, year, notes) VALUES ('1',"2009","");
+INSERT INTO section (c_id, year, notes) VALUES ('1',"2021","");
+INSERT INTO section (c_id, year, notes) VALUES ('2',"2020","");
+INSERT INTO section (c_id, year, notes) VALUES ('3',"2009","");
+
+INSERT INTO enroll (sec_id, student_id, grade, notes) VALUES ('1','1',9.50,"");
+INSERT INTO enroll (sec_id, student_id, grade, notes) VALUES ('1','2',8.50,"");
+INSERT INTO enroll (sec_id, student_id, grade, notes) VALUES ('3','1',4.00,"");
+INSERT INTO enroll (sec_id, student_id, grade, notes) VALUES ('2','1',10.00,"");
+INSERT INTO enroll (sec_id, student_id, grade, notes) VALUES ('3','2',9.00,"");
 
 INSERT INTO teaches (faculty_id, c_id, year, notes) VALUES ('1','1',"2006-06-01","");
 INSERT INTO teaches (faculty_id, c_id, year, notes) VALUES ('1','2',"2006-07-28","");
--- sql/university.ddl
+++ sql/university.ddl
@@ -8,12 +8,19 @@ CREATE TABLE `university`.`student` (
   `student_id` VARCHAR(45) NOT NULL,
   `name` VARCHAR(90) NOT NULL,
   `emailid` VARCHAR(90) NOT NULL,
+  `address` VARCHAR(200),
+  `DOB` DATE DEFAULT '2001-08-28',
   `password` VARCHAR(90) NOT NULL,
+  sem INT DEFAULT 1,
+  cpi FLOAT DEFAULT 0.00,
+  branch VARCHAR(5),
   PRIMARY KEY (`student_id`));
 
 CREATE TABLE `university`.`faculty` (
   `faculty_id` VARCHAR(45) NOT NULL,
   `name` VARCHAR(90) NOT NULL,
+  `address` VARCHAR(200),
+  `DOB` DATE DEFAULT '2001-08-28',
   `emailid` VARCHAR(90) NOT NULL,
   `password` VARCHAR(90) NOT NULL,
   PRIMARY KEY (`faculty_id`));
@@ -31,35 +38,50 @@ CREATE TABLE course (
     year DATE NOT NULL,
     notes VARCHAR(200) NOT NULL,
     PRIMARY KEY(c_id));
-    
-CREATE TABLE enroll (
+
+-- this is the current years courses, stores years
+CREATE TABLE section (
+	sec_id INT NOT NULL auto_increment,
 	c_id VARCHAR(45) NOT NULL,
+    year VARCHAR(11) DEFAULT '2021',
+    notes VARCHAR(200) NOT NULL,
+    FOREIGN KEY section(c_id) references course(c_id),
+    PRIMARY KEY(sec_id));
+
+CREATE TABLE enroll (
+	sec_id INT NOT NULL,
     student_id VARCHAR(90) NOT NULL,
     grade FLOAT NOT NULL,
     notes VARCHAR(200) NOT NULL,
-    PRIMARY KEY(student_id, c_id));
+    FOREIGN KEY enroll(sec_id) references section(sec_id),
+    FOREIGN KEY enroll(student_id) references student(student_id),
+    PRIMARY KEY(student_id, sec_id));
 
 CREATE TABLE teaches (
 	c_id VARCHAR(45) NOT NULL,
     faculty_id VARCHAR(90) NOT NULL,
     year DATE NOT NULL,
     notes VARCHAR(200) NOT NULL,
+    FOREIGN KEY teaches(c_id) references course(c_id),
+    FOREIGN KEY teaches(faculty_id) references faculty(faculty_id),
     PRIMARY KEY(faculty_id, c_id));
     
 CREATE TABLE assignment (
 	a_id INT UNIQUE NOT NULL auto_increment,
     faculty_id VARCHAR(90) NOT NULL,
-    c_id VARCHAR(90) NOT NULL,
+	c_id VARCHAR(45) NOT NULL,
     created_at DATETIME,
     end_at DATETIME,
     text VARCHAR(1000),
 	marks_total INT,
     files_link DATE,
     notes VARCHAR(200),
+    FOREIGN KEY (faculty_id) references faculty(faculty_id),
+    FOREIGN KEY (c_id) references course(c_id),
     PRIMARY KEY(a_id));
     
 CREATE TABLE submission (
-	a_id VARCHAR(45) NOT NULL,
+	a_id INT NOT NULL,
     student_id VARCHAR(90) NOT NULL,
     submission_id INT UNIQUE NOT NULL auto_increment,
     created_at DATETIME ,
@@ -69,4 +91,6 @@ CREATE TABLE submission (
     files_link DATE ,
 	marks_got INT ,
     notes VARCHAR(200) ,
+    FOREIGN KEY (a_id) references assignment(a_id),
+    FOREIGN KEY (student_id) references student(student_id),
     PRIMARY KEY(submission_id));
