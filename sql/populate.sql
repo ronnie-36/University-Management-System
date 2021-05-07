3b654005fb7155ae94bae3b6f784c3210fa4f288
Krishanus-2001 <krishanu21saini@gmail.com>
2021-05-07 13:36:42 +0530

    Add sql

--- sql/populate.sql
+++ sql/populate.sql
@@ -8,3 +8,32 @@ INSERT INTO `university`.`student` (`student_id`, `name`, `emailid`, `password`)
 INSERT INTO `university`.`admin` (`admin_id`, `name`, `emailid`, `password`) VALUES ('1', 'sqyw', 'sqyw@sqyw.com', '81dc9bdb52d04dc20036dbd8313ed055');
 INSERT INTO `university`.`admin` (`admin_id`, `name`, `emailid`, `password`) VALUES ('2', 'qwsr', 'qwsr@qwsr.com', '81dc9bdb52d04dc20036dbd8313ed055');
 
+INSERT INTO course (c_id, name, year, notes) VALUES ('1','MATHS',"2001-12-30","");
+INSERT INTO course (c_id, name, year, notes) VALUES ('2','OS',"2006-06-24","");
+INSERT INTO course (c_id, name, year, notes) VALUES ('3','DSA',"2006-06-24","");
+INSERT INTO course (c_id, name, year, notes) VALUES ('4','Software Engg',"2009-01-30","");
+INSERT INTO course (c_id, name, year, notes) VALUES ('5','IC211',"2009-01-01","");
+
+INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('1','1',9.50,"");
+INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('1','2',8.50,"");
+INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('1','3',4.00,"");
+INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('2','1',10.00,"");
+INSERT INTO enroll (c_id, student_id, grade, notes) VALUES ('2','2',9.00,"");
+
+INSERT INTO teaches (faculty_id, c_id, year, notes) VALUES ('1','1',"2006-06-01","");
+INSERT INTO teaches (faculty_id, c_id, year, notes) VALUES ('1','2',"2006-07-28","");
+INSERT INTO teaches (faculty_id, c_id, year, notes) VALUES ('2','3',"2006-06-01","");
+INSERT INTO teaches (faculty_id, c_id, year, notes) VALUES ('1','4',"2009-12-30","");
+INSERT INTO teaches (faculty_id, c_id, year, notes) VALUES ('2','5',"2010-01-30","");
+
+INSERT INTO assignment (c_id, faculty_id, created_at, end_at, text, marks_total) VALUES ('1','1',NOW(), NOW() + INTERVAL 1 DAY, 'Sort an array in O(n) using radix sort', '30');
+INSERT INTO assignment (c_id, faculty_id, created_at, end_at, text, marks_total) VALUES ('2','1',NOW() - INTERVAL 2 DAY, NOW() - INTERVAL 1 DAY, 'reverse an array in O(n)', '10');
+INSERT INTO assignment (c_id, faculty_id, created_at, end_at, text, marks_total) VALUES ('2','1',DATE_ADD(NOW(), INTERVAL 1 HOUR), DATE_ADD(NOW(), INTERVAL 3 HOUR), 'End semester exam', '60');
+INSERT INTO assignment (c_id, faculty_id, created_at, end_at, text, marks_total) VALUES ('3','2',DATE_ADD(NOW(), INTERVAL 0 HOUR), DATE_ADD(NOW(), INTERVAL 3 HOUR), 'End semester exam', '100');
+
+INSERT INTO submission (a_id, student_id, created_at, end_at, submitted_at, text, marks_got) VALUES ('1','1',NOW(), NOW() + INTERVAL 1 DAY, DATE_ADD(NOW(), INTERVAL 3 HOUR),'answer 1 text', '30');
+INSERT INTO submission (a_id, student_id, created_at, end_at, submitted_at, text, marks_got) VALUES ('2','1',NOW() - INTERVAL 2 DAY, NOW() - INTERVAL 1 DAY, DATE_ADD(NOW(), INTERVAL 1 HOUR),'answer 2 text', '10');
+INSERT INTO submission (a_id, student_id, created_at, end_at, submitted_at, text, marks_got) VALUES ('2','2',NOW(), NOW() + INTERVAL 1 DAY, DATE_ADD(NOW(), INTERVAL 3 HOUR),'answer 2 text', '5');
+
+
+
--- sql/university.ddl
+++ sql/university.ddl
@@ -24,3 +24,49 @@ CREATE TABLE `university`.`admin` (
   `emailid` VARCHAR(90) NOT NULL,
   `password` VARCHAR(90) NOT NULL,
   PRIMARY KEY (`admin_id`));
+  
+CREATE TABLE course (
+	c_id VARCHAR(45) NOT NULL,
+    name VARCHAR(90) NOT NULL,
+    year DATE NOT NULL,
+    notes VARCHAR(200) NOT NULL,
+    PRIMARY KEY(c_id));
+    
+CREATE TABLE enroll (
+	c_id VARCHAR(45) NOT NULL,
+    student_id VARCHAR(90) NOT NULL,
+    grade FLOAT NOT NULL,
+    notes VARCHAR(200) NOT NULL,
+    PRIMARY KEY(student_id, c_id));
+
+CREATE TABLE teaches (
+	c_id VARCHAR(45) NOT NULL,
+    faculty_id VARCHAR(90) NOT NULL,
+    year DATE NOT NULL,
+    notes VARCHAR(200) NOT NULL,
+    PRIMARY KEY(faculty_id, c_id));
+    
+CREATE TABLE assignment (
+	a_id INT UNIQUE NOT NULL auto_increment,
+    faculty_id VARCHAR(90) NOT NULL,
+    c_id VARCHAR(90) NOT NULL,
+    created_at DATETIME,
+    end_at DATETIME,
+    text VARCHAR(1000),
+	marks_total INT,
+    files_link DATE,
+    notes VARCHAR(200),
+    PRIMARY KEY(a_id));
+    
+CREATE TABLE submission (
+	a_id VARCHAR(45) NOT NULL,
+    student_id VARCHAR(90) NOT NULL,
+    submission_id INT UNIQUE NOT NULL auto_increment,
+    created_at DATETIME ,
+    end_at DATETIME ,
+    submitted_at DATETIME ,
+    text VARCHAR(1000) ,
+    files_link DATE ,
+	marks_got INT ,
+    notes VARCHAR(200) ,
+    PRIMARY KEY(submission_id));
